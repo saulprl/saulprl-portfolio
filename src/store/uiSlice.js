4ec35a8b5b364855f7d3849a0362e86c9c25561e
Saul <phasecross@hotmail.com>
2022-11-26 13:20:44 -0700

    Added a header title state

--- src/store/uiSlice.js
+++ src/store/uiSlice.js
@@ -4,16 +4,21 @@ const uiSlice = createSlice({
   name: "ui",
   initialState: {
     themeMode: "dark",
+    headerTitle: "Home",
   },
   reducers: {
     toggleTheme: (state) => {
       state.themeMode = state.themeMode === "dark" ? "light" : "dark";
     },
+    setHeaderTitle: (state, action) => {
+      state.headerTitle = action.payload.title;
+    },
   },
 });
 
-export const { toggleTheme } = uiSlice.actions;
+export const { toggleTheme, setHeaderTitle } = uiSlice.actions;
 
 export const selectThemeMode = (state) => state.ui.themeMode;
+export const selectHeaderTitle = (state) => state.ui.headerTitle;
 
 export default uiSlice.reducer;
