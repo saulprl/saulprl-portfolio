0482f0832bf17113273b97a88418bf51db7eb342
Saul <phasecross@hotmail.com>
2022-11-26 13:19:38 -0700

    Card styling and dynamic header

--- src/components/layout/MainContent.js
+++ src/components/layout/MainContent.js
@@ -1,14 +1,36 @@
-import { Box } from "@mui/material";
+import { useSelector } from "react-redux";
+
+import { Box, Card, CardHeader, Divider } from "@mui/material";
+
+import { selectHeaderTitle } from "../../store/uiSlice";
 
 const MainContent = (props) => {
+  const headerTitle = useSelector(selectHeaderTitle);
+
   return (
     <Box
       sx={{
+        height: { sm: "100vh" },
         width: { sm: "calc(100% - 280px)" },
         ml: { sm: "280px" },
+        padding: "1rem",
+        overflow: "auto",
       }}
     >
-      {props.children}
+      <Card
+        variant="outlined"
+        sx={{
+          minHeight: "95vh",
+          margin: "auto",
+          ml: { md: "0" },
+          borderRadius: "16px",
+          width: { xs: "100%", md: "500px" },
+        }}
+      >
+        <CardHeader title={headerTitle} />
+        <Divider />
+        {props.children}
+      </Card>
     </Box>
   );
 };
