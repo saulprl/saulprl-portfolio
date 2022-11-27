b091993fdd2b6e17ba689e6f23bcbf5d21ddd53f
Saul <phasecross@hotmail.com>
2022-11-27 13:44:55 -0700

    Added projects reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import uiReducer from "./uiSlice";
+import projectsReducer from "./projectsSlice";
 
 export default configureStore({
   reducer: {
     ui: uiReducer,
+    projects: projectsReducer,
   },
 });
