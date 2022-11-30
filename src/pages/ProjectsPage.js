64bd9d728388ab58a46364e162b8feb0da3f9faa
Saul <phasecross@hotmail.com>
2022-11-29 17:21:47 -0700

    Now renders project details component

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,11 +1,13 @@
 import { Route } from "react-router-dom";
+
+import ProjectDetails from "../components/projects/ProjectDetails";
 import Projects from "../components/projects/Projects";
 
 const ProjectsPage = () => {
   return (
     <>
       <Route path="/projects" exact render={() => <Projects />} />
-      <Route path="/projects/:id" exact />
+      <Route path="/projects/:id" render={() => <ProjectDetails />} />
     </>
   );
 };
