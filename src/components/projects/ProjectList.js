811a7575cc14e99c092a527333857a3cd7805cc0
Saul <phasecross@hotmail.com>
2022-11-29 17:22:41 -0700

    Now simulates textOverflow, Card is now clickable

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,8 +1,18 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
-import { Box, Card, List, ListItem, ListItemText } from "@mui/material";
-import { selectProjects } from "../../store/projectsSlice";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+
 import TechnologyChip from "../technologies/TechnologyChip";
+
+import { selectProjects } from "../../store/projectsSlice";
 
 const ProjectList = (props) => {
   const projects = useSelector(selectProjects);
@@ -13,37 +23,46 @@ const ProjectList = (props) => {
         <Card
           key={proj.id}
           sx={{
+            maxHeight: "136px",
             borderRadius: "8px",
             mb: "8px",
-            // display: "flex",
-            // flexDirection: "row",
           }}
         >
-          <ListItem>
-            <ListItemText primary={proj.name} secondary={proj.description} />
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-              }}
-            >
-              {proj.language.map((lang, index) => (
-                <TechnologyChip
-                  key={index}
-                  technology={lang}
-                  clickable={false}
-                  sx={{ alignSelf: "flex-end" }}
-                />
-              ))}
-              {proj.database.map((database, index) => (
-                <TechnologyChip
-                  key={index}
-                  technology={database}
-                  clickable={false}
-                />
-              ))}
-            </Box>
-          </ListItem>
+          <CardActionArea LinkComponent={Link} to={`/projects/${proj.id}`}>
+            <ListItem sx={{ alignItems: "flex-start" }}>
+              <ListItemText
+                primary={proj.name}
+                secondary={`${
+                  proj.description.length >= 166
+                    ? `${proj.description.substring(0, 166)}...`
+                    : proj.description
+                }`}
+              />
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "flex-start",
+                }}
+              >
+                {proj.language.map((lang, index) => (
+                  <TechnologyChip
+                    key={index}
+                    technology={lang}
+                    clickable={false}
+                    sx={{ alignSelf: "flex-end" }}
+                  />
+                ))}
+                {proj.database.map((database, index) => (
+                  <TechnologyChip
+                    key={index}
+                    technology={database}
+                    clickable={false}
+                  />
+                ))}
+              </Box>
+            </ListItem>
+          </CardActionArea>
         </Card>
       ))}
     </List>
