0b540a594dc043b1d87016801b614b9e0252e725
Saul <phasecross@hotmail.com>
2022-11-30 16:27:02 -0700

    Style changes and added a resume button

--- src/components/layout/MainContent.js
+++ src/components/layout/MainContent.js
@@ -1,11 +1,29 @@
 import { useSelector } from "react-redux";
 
-import { Box, Card, CardHeader, Divider } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardHeader,
+  Divider,
+  IconButton,
+  Tooltip,
+  useTheme,
+} from "@mui/material";
 
 import { selectHeaderTitle } from "../../store/uiSlice";
+import { Article } from "@mui/icons-material";
 
 const MainContent = (props) => {
+  const theme = useTheme();
   const headerTitle = useSelector(selectHeaderTitle);
+
+  const resumeClickHandler = (event) => {
+    window.open(
+      "https://drive.google.com/file/d/1k4RIq9MOmsY6oYr9CCBdv3bOMI4HqKDh/view?usp=share_link",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  };
 
   return (
     <Box
@@ -24,10 +42,21 @@ const MainContent = (props) => {
           margin: "auto",
           ml: { md: "0" },
           borderRadius: "16px",
+          border: theme.palette.border.default,
           width: { xs: "100%", md: "530px" },
         }}
       >
-        <CardHeader title={headerTitle} />
+        <CardHeader
+          title={headerTitle}
+          titleTypographyProps={{ fontWeight: "bold" }}
+          action={
+            <Tooltip title="Resume" placement="left">
+              <IconButton onClick={resumeClickHandler}>
+                <Article />
+              </IconButton>
+            </Tooltip>
+          }
+        />
         <Divider />
         {props.children}
       </Card>
