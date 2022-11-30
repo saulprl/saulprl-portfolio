b13782b954bcde1fa480155410eaaa3dd65d1a9d
Saul <phasecross@hotmail.com>
2022-11-30 13:26:51 -0700

    Added courses reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import uiReducer from "./uiSlice";
+import coursesReducer from "./coursesSlice";
 import projectsReducer from "./projectsSlice";
 
 export default configureStore({
   reducer: {
     ui: uiReducer,
+    courses: coursesReducer,
     projects: projectsReducer,
   },
 });
