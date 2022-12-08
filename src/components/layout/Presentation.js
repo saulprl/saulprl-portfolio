fc4666ae467c77651a412092eb62fc36e762768b
Saul <phasecross@hotmail.com>
2022-12-08 14:43:05 -0700

    Add socials hover color animation

--- src/components/layout/Presentation.js
+++ src/components/layout/Presentation.js
@@ -37,27 +37,36 @@ const Presentation = () => {
         <IconButton
           aria-label="twitter.com"
           onClick={() => window.open("https://twitter.com/saulpxrl", "_blank")}
-          sx={{ color: iconColor }}
+          sx={{
+            color: iconColor,
+            "&.MuiIconButton-root:hover": { color: theme.palette.info.main },
+          }}
         >
-          <Twitter />
+          <Twitter sx={{ transition: "color 275ms linear" }} />
         </IconButton>
       </Tooltip>
       <Tooltip title="GitHub">
         <IconButton
           aria-label="github.com"
           onClick={() => window.open("https://github.com/saulprl", "_blank")}
-          sx={{ color: iconColor }}
+          sx={{
+            color: iconColor,
+            "&.MuiIconButton-root:hover": { color: theme.palette.info.main },
+          }}
         >
-          <GitHub />
+          <GitHub sx={{ transition: "color 275ms linear" }} />
         </IconButton>
       </Tooltip>
       <Tooltip title="Email">
         <IconButton
           aria-label="e-mail"
           onClick={() => window.open("mailto:saulramos378@gmail.com", "_blank")}
-          sx={{ color: iconColor }}
+          sx={{
+            color: iconColor,
+            "&.MuiIconButton-root:hover": { color: theme.palette.info.main },
+          }}
         >
-          <Email />
+          <Email sx={{ transition: "color 275ms linear" }} />
         </IconButton>
       </Tooltip>
     </>
