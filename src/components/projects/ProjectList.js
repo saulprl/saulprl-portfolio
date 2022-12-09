13995f69bf91e4d404579e9e581830de4abd8278
Saul <phasecross@hotmail.com>
2022-12-08 19:50:00 -0700

    Add useMemo, useCallback, add new components

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,19 +1,9 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useMemo, useState } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
-import {
-  Box,
-  Card,
-  CardActionArea,
-  CardContent,
-  List,
-  Typography,
-  useMediaQuery,
-  useTheme,
-} from "@mui/material";
+import { List, useMediaQuery, useTheme } from "@mui/material";
 
-import TechnologyChip from "../technologies/TechnologyChip";
+import ProjectItem from "./ProjectItem";
 
 import { projects as projectsData } from "../../data/data";
 
@@ -47,85 +37,26 @@ const ProjectList = (props) => {
     });
   }, [props.filters]);
 
-  const projectItemBackground =
-    theme.palette.mode === "dark"
-      ? "default"
-      : theme.palette.background.default;
-
-  const projectList = projects.map((proj) => (
-    <CSSTransition
-      key={proj.id}
-      timeout={isMobile ? 450 : 300}
-      mountOnEnter={false}
-      classNames={{
-        enter: classes["fade-enter"],
-        enterActive: classes["fade-enter-active"],
-        exit: classes["fade-exit"],
-        exitActive: classes["fade-exit-active"],
-      }}
-    >
-      <Card
-        key={proj.id}
-        sx={{
-          background: projectItemBackground,
-          borderRadius: "8px",
-          border: theme.palette.border.default,
-          mb: "8px",
-          transition: "background-color 250ms linear, border 250ms linear",
-        }}
-        className={classes["project-item"]}
-      >
-        <CardActionArea LinkComponent={Link} to={`/projects/${proj.id}`}>
-          <CardContent>
-            <Box
-              component="div"
-              sx={{
-                display: "flex",
-                flexDirection: { xs: "column", sm: "row" },
-                justifyContent: "space-between",
-              }}
-            >
-              <Box>
-                <Typography variant="h6" fontWeight="bold">
-                  {proj.name}
-                </Typography>
-                <Typography variant="body2">{`${
-                  proj.description.length >= 166
-                    ? `${proj.description.substring(0, 166)}...`
-                    : proj.description
-                }`}</Typography>
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "flex-start",
-                  alignItems: { sm: "flex-end" },
-                }}
-              >
-                {proj.language.map((lang, index) => (
-                  <TechnologyChip
-                    key={index}
-                    technology={lang}
-                    clickable={false}
-                    selected={props.filters.includes(lang)}
-                  />
-                ))}
-                {proj.database.map((database, index) => (
-                  <TechnologyChip
-                    key={index}
-                    technology={database}
-                    clickable={false}
-                    selected={props.filters.includes(database)}
-                  />
-                ))}
-              </Box>
-            </Box>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </CSSTransition>
-  ));
+  const projectList = useMemo(
+    () =>
+      projects.map((proj) => (
+        <CSSTransition
+          key={proj.id}
+          timeout={isMobile ? 450 : 300}
+          mountOnEnter={true}
+          unmountOnExit={true}
+          classNames={{
+            enter: classes["fade-enter"],
+            enterActive: classes["fade-enter-active"],
+            exit: classes["fade-exit"],
+            exitActive: classes["fade-exit-active"],
+          }}
+        >
+          <ProjectItem project={proj} filters={props.filters} />
+        </CSSTransition>
+      )),
+    [projects, props.filters, isMobile]
+  );
 
   return (
     <List disablePadding>
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,112 +1,36 @@
-import { useState } from "react";
-import { CSSTransition } from "react-transition-group";
+import { useCallback, useMemo, useState } from "react";
 
-import {
-  Box,
-  Card,
-  CardContent,
-  CardHeader,
-  Divider,
-  IconButton,
-  useMediaQuery,
-  useTheme,
-} from "@mui/material";
-import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import { CardContent } from "@mui/material";
 
 import ProjectList from "./ProjectList";
-import TechnologyChip from "../technologies/TechnologyChip";
+import ProjectFilters from "./ProjectFilters";
 
 import { technologies as techData } from "../../data/data";
 
-import classes from "./Projects.module.css";
-
 const Projects = () => {
-  const theme = useTheme();
   const [filters, setFilters] = useState([]);
-  const technologies = { ...techData };
+  const technologies = useMemo(() => {
+    return { ...techData };
+  }, []);
 
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-  const [expanded, setExpanded] = useState(!isMobile);
-
-  const toggleContent = () => {
-    setExpanded((prevState) => !prevState);
-  };
-
-  const toggleFilter = (tech) => {
-    setFilters((prevState) => {
-      return filters.includes(tech)
-        ? [...prevState.filter((filter) => filter !== tech)]
-        : [...prevState, tech];
-    });
-  };
-
-  const techArray = [];
-  for (const key of Object.keys(technologies)) {
-    techArray.push(technologies[key]);
-  }
-
-  const techChips = techArray.map((tech) => (
-    <TechnologyChip
-      key={tech.name}
-      technology={tech}
-      clickable={true}
-      selected={filters.includes(tech)}
-      onToggle={toggleFilter}
-      sx={{ fontSize: "11px" }}
-    />
-  ));
+  const toggleFilter = useCallback(
+    (tech) =>
+      setFilters((prevState) =>
+        filters.includes(tech)
+          ? [...prevState.filter((filter) => filter !== tech)]
+          : [...prevState, tech]
+      ),
+    [filters]
+  );
 
   return (
     <>
       <CardContent>
-        <Card
-          sx={{
-            borderRadius: "8px",
-            border: theme.palette.border.default,
-            mb: "8px",
-            transition: "border 250ms linear",
-          }}
-        >
-          <CardHeader
-            title="Filters"
-            titleTypographyProps={{ variant: "h6" }}
-            action={
-              <IconButton onClick={toggleContent} sx={{ color: "white" }}>
-                <ExpandLessIcon
-                  sx={{
-                    transition: "transform 400ms",
-                    transform: `rotate(${expanded ? -180 : 0}deg)`,
-                  }}
-                />
-              </IconButton>
-            }
-            sx={{ background: theme.palette.secondary.main, color: "white" }}
-          />
-          <CSSTransition
-            in={expanded}
-            timeout={isMobile ? 450 : 300}
-            mountOnEnter={true}
-            unmountOnExit={true}
-            classNames={{
-              enter: classes["fade-enter"],
-              enterActive: classes["fade-enter-active"],
-              exit: classes["fade-exit"],
-              exitActive: classes["fade-exit-active"],
-            }}
-          >
-            <Box className={classes["filters-content"]}>
-              <Divider />
-              <CardContent
-                sx={{
-                  background: theme.palette.background.default,
-                  transition: "background-color 250ms linear",
-                }}
-              >
-                {techChips}
-              </CardContent>
-            </Box>
-          </CSSTransition>
-        </Card>
+        <ProjectFilters
+          filters={filters}
+          technologies={technologies}
+          onToggleFilter={toggleFilter}
+        />
         <ProjectList filters={filters} />
       </CardContent>
     </>
